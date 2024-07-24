92486bbd9dadcac2319506284c52030fc5c0deb0
Steven Vasquez <stevenvasq@gmail.com>
2024-07-24 00:10:51 -0400

    update app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # project_sprint_5
+La aplicacion contiene 2 bonotes en el que al activar uno u otro, se mostrará un grafico de histograma o de dispersion dependiendo de cual boton se haya activado. 
---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px 
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir histograma')
+hist_button_2 = st.button('Construir grafico de dispersion')
+
+if hist_button:
+    st.write('Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+    
+if hist_button:
+    st.write('Creacion de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+    fig_2 = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig_2)
