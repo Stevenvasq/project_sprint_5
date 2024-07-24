07f83f1a648c4a4fcc3cf9a8060d1f79cad82f73
Stevenvasq <stevenvasq@gmail.com>
2024-07-24 01:44:47 -0400

    Last changes

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ hist_button_2 = st.button('Construir grafico de dispersion')
 
 if hist_button:
     st.write('Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(car_data, x='odometer')
+    fig = px.histogram(car_data, x='model_year')
     st.plotly_chart(fig, use_container_width=True)
     
-if hist_button:
+if hist_button_2:
     st.write('Creacion de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
-    fig_2 = px.scatter(car_data, x='odometer', y='price')
+    fig_2 = px.scatter(car_data, x='model', y= 'price')
     st.plotly_chart(fig_2)
